c9548bb9e219523f0c0671856f07f8f268d61b8d
SmuSmu <SmuSmu@users.noreply.github.com>
2025-10-27 14:47:58 +0100

    Create brokenredirects.ps1
    Broken Redirects Script

---
+++ brokenredirects.ps1
@@ -0,0 +1,14 @@
+$searchTag = "strong"
+$URI = https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Defekte_Weiterleitungen&limit=500 <# broken redirects #>
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 <# using TLS 1.2 is vitally important #>
+
+$req = Invoke-Webrequest -URI $URI
+$count = ($req.ParsedHtml.getElementsByTagName($searchTag) | Select-Object -First 1).innerhtml
+
+if ($count -ge 0)
+    {
+    Write-Host Broken Redirects : $count
+    Get-Date -Format "yyyyMMdd," | Out-File -FilePath '.\broken-redirects.csv' -Encoding ascii -Append -NoNewline -Force
+    $count | Out-File -FilePath '.\broken-redirects.csv' -Encoding ascii -Append -Force
+    }
