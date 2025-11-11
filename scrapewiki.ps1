2a7936de99e56d6e0070725159d85d57791cf7e3
SmuSmu <SmuSmu@users.noreply.github.com>
2025-11-11 17:12:29 +0100

    Update and rename brokenredirects.ps1 to scrapewiki.ps1

---
+++ scrapewiki.ps1
@@ -0,0 +1,30 @@
+[Hashtable]$ToDo = @{
+    'broken-redirects.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Defekte_Weiterleitungen&limit=5000'
+    'double-redirects.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Doppelte_Weiterleitungen&limit=5000'
+    'wished-files.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Dateien&limit=5000'
+    'wished-categories.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Kategorien&limit=5000'
+    #'wished-sites.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Seiten&limit=5000'
+    'wished-templates.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Vorlagen&limit=5000'
+    }
+
+$pattern = "<strong>(.*?)</strong>"
+
+$ToDo.keys | foreach-object {
+    $File = $_
+    $URI = $ToDo.$File
+
+    Write-Host Check Site : $URI
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 <# using TLS 1.2 is vitally important #>
+
+    $req = Invoke-Webrequest -URI $URI
+
+    $count = [regex]::Match($req.RawContent,$pattern).Groups[1].Value
+
+    if ($count -ge 0)
+        {
+        Write-Host Value : $count
+        Get-Date -Format "yyyyMMdd," | Out-File -FilePath $File -Encoding ascii -Append -NoNewline -Force
+        $count | Out-File -FilePath $File -Encoding ascii -Append -Force
+        }
+    }
