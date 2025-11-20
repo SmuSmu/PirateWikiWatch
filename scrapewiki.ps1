31d3bb6a2e9df61a6e5d4e05189575ee12357958
SmuSmu <SmuSmu@users.noreply.github.com>
2025-11-20 09:13:55 +0100

    Update scrapewiki.ps1

--- scrapewiki.ps1
+++ scrapewiki.ps1
@@ -1,10 +1,10 @@
 [Hashtable]$ToDo = @{
     'broken-redirects.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Defekte_Weiterleitungen&limit=500'
-    'double-redirects.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Doppelte_Weiterleitungen&limit=5000'
-    'wished-files.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Dateien&limit=5000'
-    'wished-categories.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Kategorien&limit=5000'
-    #'wished-sites.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Seiten&limit=5000'
-    'wished-templates.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Vorlagen&limit=5000'
+    #'double-redirects.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Doppelte_Weiterleitungen&limit=5000'
+    #'wished-files.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Dateien&limit=5000'
+    #'wished-categories.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Kategorien&limit=5000'
+    #'wished-sites.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Seiten&limit=5000'  ####
+    #'wished-templates.csv' = 'https://wiki.piratenpartei.de/wiki/index.php?title=Spezial:Gew%C3%BCnschte_Vorlagen&limit=5000'
     }
 
 $pattern = "<strong>(.*?)</strong>"
